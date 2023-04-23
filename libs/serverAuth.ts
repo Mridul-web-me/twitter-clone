a40d676056c409c489acbd70895ee1ee630caa32
Mridul Islam <md.mridul.23@gmail.com>
2023-04-24 01:54:45 +0600

    user edit and image upload

--- libs/serverAuth.ts
+++ libs/serverAuth.ts
@@ -1,25 +1,29 @@
-import { NextApiRequest } from "next";
-import {getSession} from 'next-auth/react'
-
+import { NextApiRequest, NextApiResponse } from "next";
 import prisma from '@/libs/prismadb'
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
-const serverAuth = async (req: NextApiRequest)=>{
-    const session = await getSession({req});
 
-    if(!session?.user?.email){
-        throw new Error ('Not signed in');
-    }
+const serverAuth = async (req: NextApiRequest, res: NextApiResponse)=>{
+    
+    const session = await getServerSession(req, res, authOptions);
 
-    const currentUser = await prisma.user.findUnique({
-        where:{
-            email: session.user.email
+
+    if (!session?.user?.email) {
+        throw new Error('Not signed in');
+      } 
+    
+      const currentUser = await prisma.user.findUnique({
+        where: {
+          email: session.user.email,
         }
-    });
-    if(!currentUser){
-        throw new Error ('Not signed in');
-    }
-    return{ currentUser }
-}
-
-export default serverAuth;
-
+      });
+    
+      if (!currentUser) {
+        throw new Error('Not signed in');
+      }
+    
+      return { currentUser };
+    };
+    
+    export default serverAuth;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,13 @@ import LoginModals from '@/components/modals/LoginModals'
 import RegisterModals from '@/components/modals/RegistrationModals'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import EditModal from '@/components/modals/EditModal'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Toaster/>
+      <EditModal/>
     <LoginModals/>
     <RegisterModals/>
     <Layout>
